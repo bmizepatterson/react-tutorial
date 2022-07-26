5b3d1117a4516948d4d8783314796d5d6666a632
Ben Patterson <ben.patterson@avnet.com>
2022-07-26 11:40:32 -0400

    Move library functions

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { calculateWinner, squareIndexToDesc } from "./library";
 import "./index.css";
 
 function Square(props) {
@@ -119,40 +120,3 @@ class Game extends React.Component {
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
-
-function calculateWinner(squares) {
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for (let i = 0; i < lines.length; i++) {
-    const [a, b, c] = lines[i];
-    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-function squareIndexToDesc(index) {
-  let row;
-  if (index < 3) {
-    row = "top";
-  } else if (index < 6) {
-    row = "middle";
-  } else {
-    row = "bottom";
-  }
-
-  const columns = ["left", "center", "right"];
-  const column = columns[index % 3];
-
-  return `${row} ${column}`;
-}
